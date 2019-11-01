6708daf9b77a4620c3b93056369276047c134fff
Kortom-Tom <55277950+Kortom-Tom@users.noreply.github.com>
2019-11-01 09:38:01 +0100

    Add files via upload

---
+++ BMI Calculator/BMI Calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BMI_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your weight in kg:");
+            double kg = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter your height in M:");
+            double m = Convert.ToDouble(Console.ReadLine());
+
+            double BMI = kg / (m * m);
+            Console.WriteLine("Your BMI is " + Math.Round(BMI, 2));
+            Console.ReadKey();
+        }
+    }
+}
